d414618a5684b38bd406ee3324dfe7407d2272ab
mohamedslima <hammaslima@gmail.com>
2022-05-25 11:47:18 +0200

    added poster and started stars and vote

--- src/components/BoolfixFilmCard.vue
+++ src/components/BoolfixFilmCard.vue
@@ -1,32 +1,48 @@
 <template>
   <div class="boolfix_card_layout">
-      <div>{{ feature.title }} {{ feature.name }}</div>
-      <div>{{ feature.original_title }}</div>
-      <div>
-          <FlagIcons :langCode="feature.original_language"/>
-      </div>
-      <div>{{ feature.vote_average }}</div>
+    <div class="image_container">
+      <img
+        v-if="feature.poster_path !== null"
+        :src="`https://www.themoviedb.org/t/p/w342${feature.poster_path}`"
+        alt=""
+      />
+    </div>
+    <div>{{ feature.title }} {{ feature.name }}</div>
+    <div>{{ feature.original_title }} {{ feature.original_name }}</div>
+    <div>
+      <FlagIcons :langCode="feature.original_language" />
+    </div>
+    <div>
+      <span>{{ feature.vote_average }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import FlagIcons from "./FlagIcons.vue"
+import FlagIcons from "./FlagIcons.vue";
 
 export default {
-    name: "BoolfixFilmCard",
-    components: {
-        FlagIcons,
-    },
-    props: {
-        feature: Object,
-    }
-}
+  name: "BoolfixFilmCard",
+  components: {
+    FlagIcons,
+  },
+  props: {
+    feature: Object,
+  },
+  methods: {},
+};
 </script>
 
 <style lang="scss" scoped>
-    div.boolfix_card_layout {
-        margin-top: 1rem;
-        width: 280px;
-        background-color: rgba(0, 0, 0, 0.039);
+div.boolfix_card_layout {
+  width: 300px;
+  margin-top: 1rem;
+  background-color: rgba(0, 0, 0, 0.039);
+  text-align: center;
+  div.image_container {
+    img {
+      width: 240px;
     }
+  }
+}
 </style>
--- src/components/FlagIcons.vue
+++ src/components/FlagIcons.vue
@@ -23,8 +23,10 @@ export default {
           return "es";
         case "uk":
           return "ua";
-          default:
-              return "unknown";
+        case "be":
+          return "be";
+        default:
+          return "unknown";
       }
     },
   },
