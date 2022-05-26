7860060cac351c659a705fd391a8d6c0724fdf37
mohamedslima <hammaslima@gmail.com>
2022-05-26 11:37:10 +0200

    styled header

--- src/components/BoolfixMainButton.vue
+++ src/components/BoolfixMainButton.vue
@@ -3,6 +3,7 @@
           <input 
           @keyup.enter="$emit('clickedSearch', searchText)"
           v-model="searchText"
+          placeholder="Inserisci un film o una serie TV"
           type="text" 
           id="search_text">
           <button
@@ -21,6 +22,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../style/variables.scss";
 
+    div.search_and_button {
+        background-color: black;
+        text-align: right;
+        padding: 0 1rem 1rem;
+        input {
+            width: 12rem;
+            height: 2rem;
+            border: 1px solid $general-boolfix-border-color;
+        }
+        button {
+            width: 5rem;
+            height: 2rem;
+            background-color: $general-boolfix-red-color;
+            border: 2px solid $general-boolfix-border-color;
+            cursor: pointer;
+        }
+    }
 </style>
